cb4db8f41121738f8cd6370fe6107a2e8b638701
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-06-15 20:48:42 -0700

    more filled out

--- WHAT_TO_WORK_ON.js
+++ WHAT_TO_WORK_ON.js
@@ -78,18 +78,9 @@ todo - add more photos of the inside / outside of DRS
     stick clips
     chalk selection
     canyoneering ropes, bags, critter, quicklinks, etc?
-
-@ About page --------------------------------------
-
-@ Beta page ---------------------------------------
-
-@ Error page --------------------------------------
-
-@ Footer component ----------
-
-@ Guiding page ------------------------------------
-
-@ Header component ----------
+todo - is this a stupid idea: a central object with a lot of the info that could change frequently in it, that is used as the source for it through the site?
+    IE Hours of operation, resole turn around time, guide service pricing, News, etc...
+    Would be only 1 spot to go to edit the common things, instead of multiple places... hmm
 
 @ Home page ---------------------------------------
 todo - Better business name display, colors, font, size, position
@@ -104,7 +95,7 @@ todo - better style the google map on the bottom of the home page
     It currently takes up 100% width and full height
     When doing less than full height, it leaves a gap at the bottom
     ... in _mapresponsive.sass mess with aspect ratio stuff, maybe breakpoints for them?
-todo - Mention resoling drop off boxes
+todo - Mention resoling drop off boxes in another lower section, before contact info?
     - contact info
     - prices
     - other info: does approach shoes, rubber used, etc...
@@ -112,21 +103,16 @@ todo - Mention resoling drop off boxes
 todo - play with parrallax
 todo - better contact info / address styling + HTML5 style address for the addresses of shop, gym, maybe resoles? for screen readers
 
-@ Navbar component ----------
-todo - change the style of the nav links (stylesheets/layout/_navbar.sass)
-    // remove default underline
-    // remove default visited color (only for the nav at this time)
-    style hover and active together with a background and/or color change
-    style focus separate with an outline or noticable animation like an underline animating in, or border animating, etc
-
-@ Other page --------------------------------------
-    What will it be?
-
-@ Secret page -------------------------------------
-todo - replace secret section link '.' with something else, like a FA icon
-
-@ Shop page ---------------------------------------
-
+@ About page --------------------------------------
+todo - Touch up and make responsive the main story
+todo - Decide how to display personel
+    * Carousel, slower, and succinct bio for each could look great, worry the bio would be too long on mobile
+        Also probably eliminated the need for much of a background
+    * Obj with all info mapped over and cards are used to display everything
+        Should be easy to add new people and remove old ones, just get a picture, their name and a little bit of info and fill it in
+        COULD grab this info from a server... but thats server requests, and like guide roster this doesn't change that fast
+todo - figure out a nice background, maybe use some parallax
+todo - probably move nav link position of the about page to just left of 'Shop'
 
 @ Gear Page / GearCarousel ---------------------------------- https://www.npmjs.com/package/react-responsive-carousel
 //todo - add carousel to gear page
@@ -136,6 +122,72 @@ todo - FOR FINAL PRODUCTION - acquire excellent photos of the various gear we ha
 todo - FOR FINAL PRODUCTION - edit good photos to all have the same aspect ratio. 1x and 2x pixel density
 todo - FOR FINAL PRODUCTION - make smaller file sizes with different pixel densities too
 todo - FOR FINAL PRODUCTION - plug in all the images to replace the existing temporary ones
+
+@ Beta page ---------------------------------------
+todo - fill this out
+    todo - free and paid camping options for all seasons, maps to them
+        NOTE TO NOT CAMP IN CERTAIN SPOTS ?
+    todo - showers info, locations, prices
+    todo - climbing gyms in the area
+    todo - coffee shops around
+    todo - locations for wifi ?
+    todo - wet weather options beta
+        hiking, hot springs, touristy stuff, non-sandstone climbing, etc...
+
+@ Guiding page ------------------------------------
+todo - replicate / improve upon the existing graphics and info, pricing display, etc that is available on the gym site
+todo - take direction from that site i didn't even know existed !!!, images!
+todo - decide on guide bio cards or carousel
+    bio cards I could just re-use the card i develop for the employee cards, probably
+todo - gallery or carousel of photos from guiding
+    everything from beginner top roping to epic multipitch, trad instruction, not too many photos that make it seem super hot here
+
+@ Other page --------------------------------------
+    What will it be?
+    Options?
+        Courtesy page for Steve's Red Rock Resoles?
+            All the info, prices, contact info, turn around time
+        Weather?
+            Would need API access to probably multiple services, could maybe be a pain
+            Would need historic past rainfall too, which may currently be off if that sensor is still broken at RRNCA
+
+@ Shop page ---------------------------------------
+    Ambitious to have it all here and built in the same
+    More likely just link to a better than existing site re-made with a different provider, not the old school way the current one has it
+
+@ Secret page -------------------------------------
+todo - replace secret section link '.' with something else, like a FA icon
+todo - what content can go here ?
+
+@ Error page --------------------------------------
+todo - make a great one
+    slightly random error page graphic/text?
+    ladylockoff rope/gear art spelling 404 ? ha not a bad idea
+
+
+
+
+% -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Components -=-=-=-=-=-=-=-=-=-=--=-=--=-=-=-=-=-=-
+
+@ Header component ----------
+
+@ Navbar component ----------
+todo - change the style of the nav links (stylesheets/layout/_navbar.sass)
+    // remove default underline
+    // remove default visited color (only for the nav at this time)
+    style hover and active together with a background and/or color change
+    style focus separate with an outline or noticable animation like an underline animating in, or border animating, etc
+
+@ Footer component ----------
+todo - fill it out in a meaningful way, or comment it out if it seems like it is currently useless
+
+
+
+
+
+
+
+
 
 
 
