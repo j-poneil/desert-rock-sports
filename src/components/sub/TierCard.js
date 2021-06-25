4edede41010d12376aa1f61dfe86c137c074d1ee
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-06-24 17:59:30 -0700

    created, filled mostly out

---
+++ src/components/sub/TierCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const TierCard = (img, imgSm, imgAltText, imgCredit, tierName, tierDuration, tierDescription, costTable, tierNotes) => {
+    const outputCostTable = (
+        <table>
+            <thead>
+                <tr>
+                    <td>Number of Climbers</td>
+                    <td>Total Cost</td>
+                </tr>
+            </thead>
+            <tobdy>
+                {
+                    costTable.map(row => (
+                        <tr>
+                            <td>{ row.keys }</td>
+                            <td>{ row.values }</td>
+                        </tr>
+                        )
+                    )
+                }
+            </tobdy>
+        </table>
+    );
+
+    return (
+        <div>
+            {/* <picture>
+                <source srcset="extralarge.jpg" media="(min-width: 1000px)" />
+                <source srcset="large.jpg" media="(min-width: 800px)" />
+                <img srcset="medium.jpg" alt="alt text" />
+            </picture> */}
+            <picture>
+                <source srcset={ img } media="(min-width: 800px)" />
+                <img srcset={ imgSm } alt={ imgAltText } />
+            </picture>
+            <p>{ imgCredit }</p>
+            <h2>{ tierName }</h2>
+            <p>Duration: { tierDuration }</p>
+            <p>{ tierDescription }</p>
+            { outputCostTable }
+            <p>{ tierNotes }</p>
+        </div>
+    )
+}
+
+export default TierCard;
