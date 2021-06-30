adebee90a808b24173f95bbbc6560eef4e471a2d
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-06-30 16:49:30 -0700

    switched to react-bootstrap Navbar + Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,28 +4,45 @@ import { Link } from 'react-router-dom';
 // styles here:
 // src\stylesheets\layout\_navbar.sass
 
+// Just calling react-bootstraps 'Navbar' as 'NavbarRBS'
+import NavbarRBS from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
 export default function Navbar(){
     return (
-        <nav>
-            {/*
-            Plan is for on scrolling down, header gets hidden, but navbar
-            sticks to the top of the page, maybe decreasing or changing style
-            with some animation if it makes sense to do so...
+        <NavbarRBS expand="lg" bg="dark" varient="dark">
+            <NavbarRBS.Brand href="/">Desert Rock Sports</NavbarRBS.Brand> {/* //! will this work? */}
+            <NavbarRBS.Toggle aria-controls="responsive-navbar-nav" />
+            <NavbarRBS.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Item><Link to="/about">About</Link></Nav.Item>
+                    <Nav.Item><Link to="/gear">Gear</Link></Nav.Item>
+                    <Nav.Item><Link to="/beta">Beta</Link></Nav.Item>
+                    <Nav.Item><Link to="/guiding">Guiding</Link></Nav.Item>
+                    <Nav.Item><Link to="/other">Other</Link></Nav.Item>
+                    <Nav.Item><Link to="/shop">Shop</Link></Nav.Item>
+                    <Nav.Item><Link to="/secret">Secret</Link></Nav.Item>
+                    {/* Insert new navbar items above */}
+                    {/* <Nav.Item><Link to="/pagetolinkto">Page</Link></Nav.Item> */}
+                </Nav>
+            </NavbarRBS.Collapse>
+        </NavbarRBS>
 
-            Infact, perhaps header and navbar will appear to be one, then change when scrolling down...
-            */}
-            <ul id="navbar-list">
-                <li className="nav-item"><Link to="/">Home</Link></li>
-                <li className="nav-item"><Link to="/about">About</Link></li>
-                <li className="nav-item"><Link to="/gear">Gear</Link></li>
-                <li className="nav-item"><Link to="/beta">Beta</Link></li>
-                <li className="nav-item"><Link to="/guiding">Guiding</Link></li>
-                <li className="nav-item"><Link to="/other">Other</Link></li>
-                <li className="nav-item"><Link to="/shop">Shop</Link></li>
-                <li className="nav-item"><Link to="/secret">.</Link></li>
-                {/* Insert new navbar items above */}
-                {/* <li className="nav-item"><Link to="/pagetolinkto">Page</Link></li> */}
-            </ul>
-        </nav>
+        // Previous way, w/o react-bootstrap, just with react-router
+        // <nav>
+        //     <ul id="navbar-list">
+        //         <li className="nav-item"><Link to="/">Home</Link></li>
+        //         <li className="nav-item"><Link to="/about">About</Link></li>
+        //         <li className="nav-item"><Link to="/gear">Gear</Link></li>
+        //         <li className="nav-item"><Link to="/beta">Beta</Link></li>
+        //         <li className="nav-item"><Link to="/guiding">Guiding</Link></li>
+        //         <li className="nav-item"><Link to="/other">Other</Link></li>
+        //         <li className="nav-item"><Link to="/shop">Shop</Link></li>
+        //         <li className="nav-item"><Link to="/secret">.</Link></li>
+        //         {/* Insert new navbar items above */}
+        //         {/* <li className="nav-item"><Link to="/pagetolinkto">Page</Link></li> */}
+        //     </ul>
+        // </nav>
     );
 }
