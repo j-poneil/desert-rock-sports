13153a81087a1be02a320016f37c6dc788e9de88
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-06-30 20:58:11 -0700

    rough react-bootstrap grid implemented

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
+
 // determines the season and adjusts the displayed hours automatically
 // Really, this Hours component should be its own component..
 // though maybe it doesn't matter since its always going to be used with the other contact info...
@@ -39,26 +46,51 @@ function Hours(){
     );
 }
 
+
 // phone, email, address, hours
+// ! styles not used anymore
 // src/stylesheets/layout/_contact.sass
 export default function Contact(){
     return (
-        <div className="contact-flex">
-            <div className="contact-flex-item" >
-                {/* email as email link is a nice touch i should do */}
-                <p>Shop: 702-254-1143</p>
-                <p>Guide Service: 702-506-6640</p>
-                <p>Shop / Guide Service email: info@climbvegas.com</p>
+        <Container fluid>
+            <Row xl={3} lg={3} md={3} sm={3} xs={1}>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text>
+                                {/* email as email link is a nice touch i should do */}
+                                <p>Shop: 702-254-1143</p>
+                                <p>Guide Service: 702-506-6640</p>
+                                <p>Shop / Guide Service email: info@climbvegas.com</p>
 
-                <p>Gym: 702-254-5604</p>
-                <p>Gym email: sales@redrockclimbingcenter.com</p>
-            </div>
-            <div className="contact-flex-item" >
-                <p>Desert Rock Sports<br /> 8221 W Charleston Blvd<br /> Suite #102<br /> Las Vegas, NV 89117</p>
-            </div>
-            <div className="contact-flex-item" >
-                <Hours />
-            </div>
-        </div>
+                                <p>Gym: 702-254-5604</p>
+                                <p>Gym email: sales@redrockclimbingcenter.com</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text>
+                                <p>Desert Rock Sports<br /> 8221 W Charleston Blvd<br /> Suite #102<br /> Las Vegas, NV 89117</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text>
+                                <Hours />
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
