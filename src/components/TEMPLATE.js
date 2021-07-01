8b44367b688a47fd2921c752a17d588003526ab5
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-06-30 21:20:44 -0700

    added things that will be likely to be used

--- src/components/TEMPLATE.js
+++ src/components/TEMPLATE.js
@@ -2,6 +2,12 @@ import React from 'react';
 // import Styled from 'styled-components';
 // import { Carousel } from 'react-responsive-carousel';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
 // ! styles need to be made here-ish:
 // src\stylesheets\pages\_whateverpage.sass
 // ! AND imported into:
@@ -9,8 +15,13 @@ import React from 'react';
 
 export default function NameOfPage(){
     return (
-        <div>
-            Page Content
-        </div>
+        <Container fluid>
+            <Row xl={2} lg={2} md={2} sm={2} xs={1}>
+                <Col></Col>
+                <Col></Col>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+        </Container>
     );
 }
