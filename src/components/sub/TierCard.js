ccd35b16fe70a7fe4bcfb83efbbde5cc2cd2e38d
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-06-30 19:01:26 -0700

    initial transition to react-bootstrap Cards

--- src/components/sub/TierCard.js
+++ src/components/sub/TierCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Table from 'react-bootstrap/Table'; // ! -- REACT-BOOTSTRAP
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 
 const TierCard = ({ img, imgSm, imgAltText, imgCredit, tierName, tierDuration, tierDescription, costTable, tierNotes }) => {
     const outputCostTable = (
@@ -26,23 +27,43 @@ const TierCard = ({ img, imgSm, imgAltText, imgCredit, tierName, tierDuration, t
     );
 
     return (
-        <div>
-            {/* <picture>
-                <source srcSet="extralarge.jpg" media="(min-width: 1000px)" />
-                <source srcSet="large.jpg" media="(min-width: 800px)" />
-                <img srcSet="medium.jpg" alt="alt text" />
-            </picture> */}
-            <picture>
-                <source srcSet={ img } media="(min-width: 800px)" />
-                <img src={ imgSm } alt={ imgAltText } />
-            </picture>
-            <p>{ imgCredit }</p>
-            <h2>{ tierName }</h2>
-            <p>Duration: { tierDuration }</p>
-            <p>{ tierDescription }</p>
-            { outputCostTable }
-            <p>{ tierNotes }</p>
-        </div>
+        // react-bootstrap way
+        <Card className="text-center">
+            <Card.Img varient="top" src={ imgSm } alt={ imgAltText } />
+            <Card.ImgOverlay>
+                <Card.Text>{ imgCredit }</Card.Text>
+            </Card.ImgOverlay>
+            <Card.Body>
+                <Card.Title>{ tierName }</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Duration: { tierDuration }</Card.Subtitle>
+                <Card.Text className="text-danger"> {/* // ! remember to add descriptions! */}
+                    { tierDescription }
+                </Card.Text>
+                { outputCostTable }
+                <Card.Text>
+                    { tierNotes }
+                </Card.Text>
+            </Card.Body>
+        </Card>
+
+        // old way
+        // <div>
+        //     {/* <picture>
+        //         <source srcSet="extralarge.jpg" media="(min-width: 1000px)" />
+        //         <source srcSet="large.jpg" media="(min-width: 800px)" />
+        //         <img srcSet="medium.jpg" alt="alt text" />
+        //     </picture> */}
+        //     <picture>
+        //         <source srcSet={ img } media="(min-width: 800px)" />
+        //         <img src={ imgSm } alt={ imgAltText } />
+        //     </picture>
+        //     <p>{ imgCredit }</p>
+        //     <h2>{ tierName }</h2>
+        //     <p>Duration: { tierDuration }</p>
+        //     <p>{ tierDescription }</p>
+        //     { outputCostTable }
+        //     <p>{ tierNotes }</p>
+        // </div>
     )
 }
 
