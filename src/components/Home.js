f50a324b2cc73b287ba1b47d366b0b0f5de5bf86
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-06-30 20:57:53 -0700

    minor

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 
 // sub components
