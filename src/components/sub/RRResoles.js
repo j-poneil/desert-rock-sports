7bc8f6eea4a6d1bcafcc43f4a6ae0a215e3234ad
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-06-30 20:58:22 -0700

    rough react-bootstrap grid implemented

--- src/components/sub/RRResoles.js
+++ src/components/sub/RRResoles.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
+// ! no longer used
 // src\stylesheets\layout\_layout_containers.sass
 // src\stylesheets\components\_rrresoles.sass
 
@@ -14,37 +21,66 @@ import React from 'react';
 
 function RRResoles() {
     return (
-        <div className="twogrid-container">
-            <div className="rrresoles-text">
-                <h1>Get some fresh rubber on those shoes!</h1>
-                <p>Steve Krall is now your source for a Vegas local resoler. Excellent reviews, industry standard prices, and reasonable turn around time. You don't even need to ship them out!</p>
-                <h3>How it works:</h3>
-                <p>Drop them off at the convient box in Desert Rock Sports (other locations exist too!). Text Steve the make/model and size of each pair that you drop off, take a picture too if you would like. Thats it!</p>
-                <p>Just wait to hear back from him about what he thinks the shoes need and the cost, then you pay him with cash, check, or venmo (@resoleSteve), and he drops them back off at Desert Rock Sports when they are finished. (He can ship too if needed).</p>
-            </div>
-            <div className="rrresoles-gallery">
-                ( a gallery or carousel of before/after images )
-            </div>
-            <div className="rrresoles-cost">
-                <h1>Prices:</h1>
-                <ul>
-                    <li>Half Sole: $40 / Pair</li>
-                    <li>Rand Repair: $10 / Each Shoe</li>
-                    <li>Approach Shoes / Other: Discuss with him</li>
-                    <li>Rush Charge: $20 / Pair</li>
-                </ul>
-                <p>He uses Unparallel Rubber (Stealth C4 equivalent)</p>
-            </div>
-            <div className="rrresoles-contact">
-                <h1>Contact Info:</h1>
-                <ul>
-                    <li>Steve Krall</li>
-                    <li>Call / Text: 702-277-1957</li>
-                    <li>IG: @redrockresoles</li>
-                    <li>Email: steve@redrockresoles.com</li>
-                </ul>
-            </div>
-        </div>
+        <Container fluid>
+            <Row xl={2} lg={2} md={2} sm={2} xs={1}>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Red Rock Resoles</Card.Title>
+                            <Card.Subtitle>Steve Krall</Card.Subtitle>
+                            <Card.Text>
+                                <h1>Get some fresh rubber on those shoes!</h1>
+                                <p>Steve Krall is now your source for a Vegas local resoler. Excellent reviews, industry standard prices, and reasonable turn around time. You don't even need to ship them out!</p>
+                                <h3>How it works:</h3>
+                                <p>Drop them off at the convient box in Desert Rock Sports (other locations exist too!). Text Steve the make/model and size of each pair that you drop off, take a picture too if you would like. Thats it!</p>
+                                <p>Just wait to hear back from him about what he thinks the shoes need and the cost, then you pay him with cash, check, or venmo (@resoleSteve), and he drops them back off at Desert Rock Sports when they are finished. (He can ship too if needed).</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img src="" alt="" />
+                        <Card.Body>
+                            <Card.Title>Previous Work:</Card.Title>
+                            <Card.Text>( a gallery or carousel of before/after images )</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Prices:</Card.Title>
+                            <Card.Text>
+                                <ul>
+                                    <li>Half Sole: $40 / Pair</li>
+                                    <li>Rand Repair: $10 / Each Shoe</li>
+                                    <li>Approach Shoes / Other: Discuss with him</li>
+                                    <li>Rush Charge: $20 / Pair</li>
+                                </ul>
+                                <p>He uses Unparallel Rubber (Stealth C4 equivalent)</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Contact Info:</Card.Title>
+                            <Card.Text>
+                                <ul>
+                                    <li>Steve Krall</li>
+                                    <li>Call / Text: 702-277-1957</li>
+                                    <li>IG: @redrockresoles</li>
+                                    <li>Email: steve@redrockresoles.com</li>
+                                </ul>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
