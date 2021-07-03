945beb3b968f66f94f9f29a1e0f4946539f0008e
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-07-03 11:43:56 -0700

    added guide bio photos, some bios, etc

--- src/components/data/guides.js
+++ src/components/data/guides.js
@@ -1,6 +1,19 @@
 // % photo imports
-// Have not decided on what dimensions should be standardized, probably something like 250 x 250px
+// Standardized to 450 x 450px, kinda arbitrary, but whatever. Not too big.
 // import SOMEPHOTO from '../../img/RRCG/GuideBioImgs/SOMEPHOTO.JPG';
+import Matt from '../../img/RRCG/GuideBioImgs/Matt_450w_450h.jpg';
+import T2 from '../../img/RRCG/GuideBioImgs/T2_450w_450h.jpg';
+import Elaina from '../../img/RRCG/GuideBioImgs/Elaina_450w_450h.jpg';
+import Rachel from '../../img/RRCG/GuideBioImgs/Rachel_450w_450h.jpg';
+import Samantha from '../../img/RRCG/GuideBioImgs/Samantha_450w_450h.jpg';
+import Tracy from '../../img/RRCG/GuideBioImgs/Tracy_450w_450h.jpg';
+import Cody from '../../img/RRCG/GuideBioImgs/Cody_450w_450h.jpg';
+import Josh from '../../img/RRCG/GuideBioImgs/Josh_450w_450h.jpg';
+import Danny from '../../img/RRCG/GuideBioImgs/Danny_450w_450h.jpg';
+import Dan from '../../img/RRCG/GuideBioImgs/Dan_450w_450h.jpg';
+import Adam from '../../img/RRCG/GuideBioImgs/Adam_450w_450h.jpg';
+import Joanne from '../../img/RRCG/GuideBioImgs/Joanne_450w_450h.jpg';
+import Nathan from '../../img/RRCG/GuideBioImgs/Nathan_450w_450h.jpg';
 
 // Used in Guiding.js
 export const guides = [
@@ -11,8 +24,9 @@ export const guides = [
         bio1: "",
         bio2: "",
         bio3: "",
-        pic: "https://via.placeholder.com/500x400",
+        pic: Dan,
         ig: "",
+        site: '',
         active: true
     },
     {
@@ -22,8 +36,9 @@ export const guides = [
         bio1: "",
         bio2: "",
         bio3: "",
-        pic: "https://via.placeholder.com/500x400",
-        ig: "",
+        pic: Josh,
+        ig: "https://www.instagram.com/tullynation/",
+        site: '',
         active: true
     },
     {
@@ -33,30 +48,9 @@ export const guides = [
         bio1: "From ATX, couldn't resist the draw of Red Rock Sandstone.",
         bio2: "Loves easy multipitch, cracks, offwidths.",
         bio3: "Coded this site. Also sews.",
-        pic: "https://via.placeholder.com/500x400",
+        pic: T2,
         ig: "https://www.instagram.com/climbguidecraft/",
-        active: true
-    },
-    {
-        name: "Adam Happensack",
-        certs: "",
-        exp: "",
-        bio1: "",
-        bio2: "",
-        bio3: "",
-        pic: "https://via.placeholder.com/500x400",
-        ig: "https://www.instagram.com/a.happensack/",
-        active: true
-    },
-    {
-        name: "Tracy Martin",
-        certs: "",
-        exp: "",
-        bio1: "",
-        bio2: "",
-        bio3: "",
-        pic: "https://via.placeholder.com/500x400",
-        ig: "https://www.instagram.com/tracypmartin/",
+        site: '',
         active: true
     },
     {
@@ -66,8 +60,45 @@ export const guides = [
         bio1: "",
         bio2: "",
         bio3: "",
-        pic: "https://via.placeholder.com/500x400",
+        pic: Rachel,
         ig: "https://www.instagram.com/twincannons/",
+        site: '',
+        active: true
+    },
+    {
+        name: "Tracy Martin",
+        certs: "AMGA Certified Rock Guide, WFR",
+        exp: "",
+        bio1: "Tracy likes to rock climb and create art. But because she must work, she splits her time between being a certified AMGA Rock Climbing Guide, Graphic Artist and as a struggling arteest. She lives and plays in Las Vegas, NV with her 2 kitties, Oscar and Bounce.",
+        bio2: "",
+        bio3: "",
+        pic: Tracy,
+        ig: "https://www.instagram.com/tracypmartin/",
+        site: 'https://tracypmartin.com/',
+        active: true
+    },
+    {
+        name: "Elaina Arenz",
+        certs: "AMGA Certified Rock Guide, AMGA Certified Rock Instructor, AMGA SPI Provider Trainer, WFR, IRATA/SPRAT Level 2 Rope Access Technician, Dirt Biker Gumby",
+        exp: "Over 24 years",
+        bio1: "Elaina is one of the co-owners of Chicks Climbing & Skiing and owner/operator of New River Mountain Guides. She has pursued climbing as a career for over 24 years. She's traveled all over North America and Europe and was instrumental in establishing El Potrero Chico, Mexico.",
+        bio2: "Las Vegas, NV is her home base and she guides almost year-round in Red Rock. During the hottest/coldest months she travels to work rock and ice programs for Chicks Climbing & Skiing, teach SPI Courses and manages things back at the New River Gorge, WV. She is a newly minted AMGA SPI Provider Trainer as of 2018 and has worked as a Warrior's Way Trainer since 2006. The Warrior's Way is a method of approaching climbing which helps one to stay focused to overcome fear of falling and improve commitment in climbing. Elaina served on the Board of Directors of the Access Fund 2014-2017 and has supported their efforts for the last 20 years as an Athlete Ambassador. In 2001 she was awarded the Sharp End Award by The Access Fund for her outstanding contribution to climbers’ advocacy.",
+        bio3: "Elaina has appeared in the Women of Climbing Calendar in 2001 and 2003 and is also featured in photographer Chris Noble’s book called Women Who Dare: North America's Most Inspiring Women Climbers, alongside her climbing heroes Lynn Hill and Kitty Calhoun.",
+        pic: Elaina,
+        ig: "https://www.instagram.com/lainersclimbs/",
+        site: '',
+        active: true
+    },
+    {
+        name: "Samantha Weichert",
+        certs: "AMGA SPI, has taken the Rock Guide Course, WFR",
+        exp: "",
+        bio1: "",
+        bio2: "",
+        bio3: "",
+        pic: Samantha,
+        ig: "https://www.instagram.com/sam.weich/",
+        site: '',
         active: true
     },
     {
@@ -77,30 +108,57 @@ export const guides = [
         bio1: "",
         bio2: "",
         bio3: "",
-        pic: "https://via.placeholder.com/500x400",
+        pic: Joanne,
         ig: "",
+        site: '',
         active: true
     },
     {
-        name: "Elaina Arenz",
-        certs: "AMGA Certified Rock Guide, IRATA/SPRAT Level 2 Rope Access Technician, Dirt Biker Gumby",
-        exp: "",
-        bio1: "",
-        bio2: "",
-        bio3: "",
-        pic: "https://via.placeholder.com/500x400",
-        ig: "https://www.instagram.com/lainersclimbs/",
-        active: true
-    },
-    {
-        name: "Samantha Weichert",
+        name: "Matt Kuehl",
         certs: "",
         exp: "",
         bio1: "",
         bio2: "",
         bio3: "",
-        pic: "https://via.placeholder.com/500x400",
-        ig: "https://www.instagram.com/sam.weich/",
+        pic: Matt,
+        ig: "https://www.instagram.com/mattkuehlphoto/",
+        site: 'https://www.mattkuehlphoto.com/',
+        active: true
+    },
+    {
+        name: "Cody Bradford",
+        certs: "AMGA Certified Rock Guide, WFR",
+        exp: "?+ years climbing, 9+ Guiding",
+        bio1: "I began my career on the sculpted meta-sandstone escarpments of the North Carolina High Country at the height of the Eastern Continental Divide. I received my Bachelors of Science in Recreation Management from Appalachian State University in 2012 and subsequently began my guiding career by earning my AMGA Single Pitch Instructor certification later that year.",
+        bio2: "I currently reside in Las Vegas, guiding out of Red Rock National Conservation Area, Joshua Tree National Park, the Sierra, and Moab. During the summer, I spend my time guiding in the Cascades as well as Wyoming in locations such as the Tetons and the Winds.",
+        bio3: "Musician by night.",
+        pic: Cody,
+        ig: "https://www.instagram.com/thecodybradford/",
+        site: 'https://www.codybradford.com/',
+        active: true
+    },
+    {
+        name: "Nathan Petrosian",
+        certs: "",
+        exp: "",
+        bio1: "",
+        bio2: "",
+        bio3: "",
+        pic: Nathan,
+        ig: "",
+        site: '',
+        active: true
+    },
+    {
+        name: "Adam Happensack",
+        certs: "",
+        exp: "",
+        bio1: "",
+        bio2: "",
+        bio3: "",
+        pic: Adam,
+        ig: "https://www.instagram.com/a.happensack/",
+        site: '',
         active: true
     },
     {
@@ -110,8 +168,9 @@ export const guides = [
         bio1: "",
         bio2: "",
         bio3: "",
-        pic: "https://via.placeholder.com/500x400",
+        pic: Danny,
         ig: "https://www.instagram.com/dynooftheshire/",
+        site: '',
         active: true
     }
 ];
