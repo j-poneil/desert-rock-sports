eae55cba30efdb96c3b9c9dd604b616337344d1c
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-07-04 12:17:34 -0700

    scaffolding

--- src/components/Beta.js
+++ src/components/Beta.js
@@ -12,20 +12,195 @@ import Image from 'react-bootstrap/Image';
 // ? - Accordian? Cards? Modals? Tabs?
 
 
-// Hiking, Canyoneering, Showers, Free Camping, Paid Camping, Trail Running, MTB Trails, Food Beta, Laundry, Coffee Shops, Seasonal Area recommendations (summer shade/altitude, winter sun), rainy day options, Route recommendations per style?, 
+// Route recommendations per style?,
+// ----------------------
+// Red Rock General Info
+// other
+// Paid Camping
+// Free Camping
+// Showers
+// Laundry
+// Coffee Shops
+// Restraunts
+// Hiking
+// Trail Running
+// Mountain biking
+// Canyoneering
+// Shady options
+// Sunny options
+// Raindy day / rock needs to dry options
+// Crack options
+// Offwidth options
 
 export default function Beta(){
     return (
         <Container fluid>
             <Row xl={3} lg={3} md={2} sm={2} xs={1}>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Red Rock General Info</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>other?</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Paid Camping</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Free Camping</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card></Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Showers</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Laundry</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Coffee Shops</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Restraunts</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Hiking</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Trail Running</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Mountain Biking</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Canyoneering</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Shady Options</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Sunny Options</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Rainy day / dryout day options</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Crack Options</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Offwidth Options</Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                    {/* <Card>
+                        <Card.Body>
+                            <Card.Title></Card.Title>
+                            <Card.Text></Card.Text>
+                        </Card.Body>
+                    </Card> */}
             </Row>
         </Container>
     );
