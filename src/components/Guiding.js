f7b683ee04fec90ab7a6ba64f8cd612dbe488df0
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-07-03 23:10:34 -0700

    jumbotron, IG icon, card

--- src/components/Guiding.js
+++ src/components/Guiding.js
@@ -13,6 +13,7 @@ import { tiers } from './data/guideTiers';
 import { guides } from './data/guides';
 
 import TierCard from './sub/TierCard';
+import { Jumbotron } from 'react-bootstrap';
 
 // 'https://via.placeholder.com/500x400'
 
@@ -54,14 +55,13 @@ export default function Guiding(){
                     <Card>
                         <Card.Body>
                             <Card.Img src={i.pic} alt={i.name} />
-                            <Card.Title className="text-center">{ i.name }</Card.Title>
+                            <Card.Title className="text-center">{ i.name } {i.ig !== "" && <a href={i.ig}><FaInstagram /></a>}</Card.Title>
                             <Card.Subtitle className="mb-2 text-muted text-center">{ i.certs !== "" && i.certs }</Card.Subtitle>
                             <Card.Text>
                                 {i.exp !== "" && <p>{i.exp}</p>}
                                 <p>{i.bio1}</p>
                                 {i.bio2 !== "" && <p>{i.bio2}</p>}
                                 {i.bio3 !== "" && <p>{i.bio3}</p>}
-                                {i.ig !== "" && <a href={i.ig}><FaInstagram /></a>}
                                 {i.site !== "" && <><br/><a href={i.site}>personal site</a></>}
                             </Card.Text>
                         </Card.Body>
@@ -72,18 +72,25 @@ export default function Guiding(){
     });
 
     return (
-        <div>
+        <Container fluid className="mt-3 mb-3">
             <Container fluid>
                 <Row xl={2} lg={2} md={2} sm={1} xs={1}>
                     <Col>
-                        Red Rock Climbing Guides
+                        <h1 className="">
+                            Red Rock Climbing Guides
+                        </h1>
                     </Col>
                     <Col>
-                        <p>The entire staff of the Red Rock Climbing Guides (RRCG) believes our responsibility to the climbing community does not begin and end at our front door. We recognize it extends across the globe, anywhere our climbers are climbing.</p>
-                        <p>We are committed to providing a safe outdoor rock climbing experience regardless of your ability. Our goal is to help you develop your rock-craft skills while becoming a knowledgeable, efficient, environmentally-aware climber focused on safety and leave-no-trace ethics.</p>
-                        <p>All of our activities are designed to help you develop self-reliance, teamwork and self-confidence on the rock. After completing any of our activities, you will be a better, more competent climber.</p>
-                        <p>Red Rock Climbing Guides operate in the Red Rock Canyon National Conservation Area by permission of the Las Vegas District of the Bureau of Land Management. We are Las Vegas' only locally-owned guide service and are prepared to make your outdoor experience Fun, Affordable, and Safe!</p>
-                        <p>We provide all your technical equipment! You only need to bring food, water, a decent pair of shoes for hiking in, and clothing appropriate for the season and activity. Call us if you need assistance.</p>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title></Card.Title>
+                                <Card.Text>The entire staff of the Red Rock Climbing Guides (RRCG) believes our responsibility to the climbing community does not begin and end at our front door. We recognize it extends across the globe, anywhere our climbers are climbing.</Card.Text>
+                                <Card.Text>We are committed to providing a safe outdoor rock climbing experience regardless of your ability. Our goal is to help you develop your rock-craft skills while becoming a knowledgeable, efficient, environmentally-aware climber focused on safety and leave-no-trace ethics.</Card.Text>
+                                <Card.Text>All of our activities are designed to help you develop self-reliance, teamwork and self-confidence on the rock. After completing any of our activities, you will be a better, more competent climber.</Card.Text>
+                                <Card.Text>Red Rock Climbing Guides operate in the Red Rock Canyon National Conservation Area by permission of the Las Vegas District of the Bureau of Land Management. We are Las Vegas' only locally-owned guide service and are prepared to make your outdoor experience Fun, Affordable, and Safe!</Card.Text>
+                                <Card.Text>We provide all your technical equipment! You only need to bring food, water, a decent pair of shoes for hiking in, and clothing appropriate for the season and activity. Call us if you need assistance.</Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
@@ -117,6 +124,6 @@ export default function Guiding(){
                 </Card>
             </Container>
             
-        </div>
+        </Container>
     );
 }
