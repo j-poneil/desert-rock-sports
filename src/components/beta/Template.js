602eda027afcc5b141b0a0055f2831eb4ce5c039
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-07-12 11:13:22 -0700

    added bg image size notes to template

--- src/components/beta/Template.js
+++ src/components/beta/Template.js
@@ -12,14 +12,20 @@ import Button from 'react-bootstrap/Button';
 // import Photo from '../../img/beta/photo.jpg';
 
 // styles
+// backgroundImage: `url('https://via.placeholder.com/50')`,
 const titleCardStyles = {
+    // a eye-catching, interesting image for the Card / Section Header
+    // size?
+    // ~650px wide for large screens would shrink it, usually
+    // ~400px wide for medium
+    // ~300px wide for small, xs
+    // seems like thats a decent collection of options?
+    // BONUS, use SVG graphics so no distortion on different screen sizes???
     backgroundImage: `url('https://via.placeholder.com/50')`,
-    // switch to 'cover' with a real image, probably and/or have multiple images for multiple screen sizes
     // backgroundRepeat: 'no-repeat',
-    // but a pattern background repeated, could look nice on any screen size... BUT maybe not as nice...
+    // switch from 'contain' to 'cover' with a real image, probably and/or have multiple images for multiple screen sizes
     backgroundSize: 'contain',
     color: 'white',
-    // textShadow
     textShadow: '-1px 1px 0 black, 1px 1px 0 black, 1px -1px 0 black, -1px -1px 0 black'
 };
 
@@ -27,7 +33,9 @@ export default function Template() {
     const [ accordionVisible, setAccordionVisible ] = useState(false);
     const clickHandler = (e) => {
         e.preventDefault();
-        return accordionVisible ? setAccordionVisible(false) : setAccordionVisible(true);
+        return accordionVisible ?
+            setAccordionVisible(false)
+            : setAccordionVisible(true);
     };
 
     // @ instead of "as={Button}" I may be able to do role="button"... try it out
@@ -37,9 +45,14 @@ export default function Template() {
                 <Card.Body
                     as={Button}
                     onClick={clickHandler}
+                    //@ will this line work to add/remove the aria-expanded property? Whats the effect of null here?
+                    { ...accordionVisible ? 'aria-expanded' : null }
                     style={titleCardStyles}>
-                    <Card.Title>{accordionVisible ? <FaRegCaretSquareUp /> : <FaRegCaretSquareDown />} Card/Section Title </Card.Title>
-                    <div className="sr-only sr-only-focusable">Show/Hide Accordion Sub-Menu</div>
+                    <Card.Title role="heading">
+                        {accordionVisible ? <FaRegCaretSquareUp /> : <FaRegCaretSquareDown />}
+                         Card/Section Title
+                    </Card.Title>
+                    <span className="sr-only sr-only-focusable">Show/Hide Accordion Sub-Menu</span>
                 </Card.Body>
             </Card>
             
