5ba58debe6d2ad4cac1823b9e1a582232979bf77
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-08-18 21:01:28 -0700

    more readable JSX formatting

--- src/components/data/newsItems.js
+++ src/components/data/newsItems.js
@@ -9,19 +9,32 @@ import React from 'react';
 // title, date, post
 // The post part itself will be interpreted as HTML, so you can have like %amp; for '&', can add links, images, etc...
 export const newsItems = [
+    // {
+    //     title: "Title",
+    //     date: "07/21/2021",
+    //     post: () => (
+    //         <>
+    //             <p>Content.</p>
+    //         </>
+    //     )
+    // },
     {
         title: "Did you know we are a dealer for Red Paddle Co?",
         date: "07/21/2021",
-        post: () => <>They have some of the highest quality paddleboards in the game. Unfortunely we don't keep them in stock, but, if you are looking to buy, we can take down your order and bundle it with a few other orders to ship to our store at the same time.</>
+        post: () => (
+            <>
+                <p>They have some of the highest quality paddleboards in the game. Unfortunely we don't keep them in stock, but, if you are looking to buy, we can take down your order and bundle it with a few other orders to ship to our store at the same time.</p>
+            </>
+        )
     },
     {
         title: "New website is coming along nicely",
         date: "07/20/2021",
-        post: () => <>Working on styling, UI/UX, etc...</>
+        post: () => (<>Working on styling, UI/UX, etc...</>)
     },
     {
         title: "Some inspirational title!",
         date: "01/01/2020",
-        post: () => <>Its been a long January. So many interesting changes int he shop. Can't wait to tell everyone about them! So much new equipment! So little time! So many routes to climb! RR 4 Life!</>
+        post: () => (<>Its been a long January. So many interesting changes int he shop. Can't wait to tell everyone about them! So much new equipment! So little time! So many routes to climb! RR 4 Life!</>)
     }
 ];
--- src/components/data/staffList.js
+++ src/components/data/staffList.js
@@ -28,7 +28,11 @@ export const staff = [
     {
         name: "Travis Graves",
         position: "",
-        bio: () => <><p>continues to be the store buyer and manager. He is the person you will most likely see or talk to on the phone. He is a wealth of information about outdoor gear and the local climbing routes. The store is small, but Travis makes sure the store is well stocked in gear, shoes and clothing for camping, backpacking and canyoneering, as well as for climbing.</p></>,
+        bio: () => (
+            <>
+                <p>continues to be the store buyer and manager. He is the person you will most likely see or talk to on the phone. He is a wealth of information about outdoor gear and the local climbing routes. The store is small, but Travis makes sure the store is well stocked in gear, shoes and clothing for camping, backpacking and canyoneering, as well as for climbing.</p>
+            </>
+        ),
         imgSrc: TG,
         ig: "https://www.instagram.com/travis_graves/",
         site: '',
@@ -38,7 +42,11 @@ export const staff = [
     {
         name: "Mike Lorenzo",
         position: "",
-        bio: () => <><p>who came to Las Vegas from New York about ten years ago, enjoys hiking, skiing, snowboarding and climbing. Mike is also a student of Yoga. Mike says that Yoga has given him the flexibility and strength he needs for climbing.</p></>,
+        bio: () => (
+            <>
+                <p>who came to Las Vegas from New York about ten years ago, enjoys hiking, skiing, snowboarding and climbing. Mike is also a student of Yoga. Mike says that Yoga has given him the flexibility and strength he needs for climbing.</p>
+            </>
+        ),
         imgSrc: Mike,
         ig: "https://www.instagram.com/positivevibe75/",
         site: '',
@@ -48,7 +56,12 @@ export const staff = [
     {
         name: "Steve Mallory",
         position: "",
-        bio: () => <><p>has a background in finance and helps with the bookkeeping. He has been climbing since 1979 and has lived in Las Vegas for 22 years. Steve recently developed a passion for canyoneering and has added Imlay Canyon Gear, static rope and the La Sportiva Exum River Shoe, to the store's inventory.</p><p>Canyoneering in Zion is a great way to beat the heat in Las Vegas during the summer months!</p></>,
+        bio: () => (
+            <>
+                <p>has a background in finance and helps with the bookkeeping. He has been climbing since 1979 and has lived in Las Vegas for 22 years. Steve recently developed a passion for canyoneering and has added Imlay Canyon Gear, static rope and the La Sportiva Exum River Shoe, to the store's inventory.</p>
+                <p>Canyoneering in Zion is a great way to beat the heat in Las Vegas during the summer months!</p>
+            </>
+        ),
         imgSrc: Steve,
         ig: "",
         site: '',
@@ -58,7 +71,7 @@ export const staff = [
     {
         name: "Dan Young",
         position: "",
-        bio: () => <></>,
+        bio: () => (<></>),
         imgSrc: Dan,
         ig: "",
         site: '',
@@ -68,7 +81,13 @@ export const staff = [
     {
         name: "Joshua Janes",
         position: "",
-        bio: () => <><p>Josh's passion for climbing spans several decades and is rooted in love for adventure, connection to the natural world, and the sheer joy and challenge of movement and problem solving. This passion has led him to climb and guide throughout the US as well as in Canada, Mexico, Europe, and Australia. He has established numerous single- and multi-pitch first ascents up to 5.13, has replaced countless aging bolts as an ASCA volunteer, and is an active contributor to the Mountain Project.</p><p>Josh has called Red Rocks home for over ten years and knows the area intimately. He is one of few who have guided the big trifecta of the Rainbow Wall, Levitation 29, and Cloud Tower - each multiple times - but perhaps his favorite climb is Epinephrine (which he belives to be the best 5.9 in the world). Most of all he enjoys sharing an adventure with his clients with the hope of building a lasting climbing partnership.</p><p>Josh is a Wilderness First Responder, enjoys science fiction, margaritas, and shares his life with his partner and their golden retriever.</p></>,
+        bio: () => (
+            <>
+                <p>Josh's passion for climbing spans several decades and is rooted in love for adventure, connection to the natural world, and the sheer joy and challenge of movement and problem solving. This passion has led him to climb and guide throughout the US as well as in Canada, Mexico, Europe, and Australia. He has established numerous single- and multi-pitch first ascents up to 5.13, has replaced countless aging bolts as an ASCA volunteer, and is an active contributor to the Mountain Project.</p>
+                <p>Josh has called Red Rocks home for over ten years and knows the area intimately. He is one of few who have guided the big trifecta of the Rainbow Wall, Levitation 29, and Cloud Tower - each multiple times - but perhaps his favorite climb is Epinephrine (which he belives to be the best 5.9 in the world). Most of all he enjoys sharing an adventure with his clients with the hope of building a lasting climbing partnership.</p>
+                <p>Josh is a Wilderness First Responder, enjoys science fiction, margaritas, and shares his life with his partner and their golden retriever.</p>
+            </>
+        ),
         imgSrc: Josh,
         ig: "https://www.instagram.com/tullynation/",
         site: '',
@@ -78,7 +97,7 @@ export const staff = [
     {
         name: "Lisa Buchina",
         position: "",
-        bio: () => <></>,
+        bio: () => (<></>),
         imgSrc: LisaB,
         ig: "https://www.instagram.com/lmbuch/",
         site: '',
@@ -88,7 +107,7 @@ export const staff = [
     {
         name: "Lisa Aquino",
         position: "",
-        bio: () => <></>,
+        bio: () => (<></>),
         imgSrc: "https://via.placeholder.com/500x400",
         ig: "https://www.instagram.com/monilisa.aquino/",
         site: '',
@@ -98,7 +117,15 @@ export const staff = [
     {
         name: "Travis O'Neil (aka T2)",
         position: "",
-        bio: () => <><p>Originally from the humidity and short polished limestone of ATX and the JTree-like granite of Enchanted Rock... once he got a taste of what the great sandstone of Red Rock could offer, he knew he had to make it his home.</p><p>Loves easy multipitch, cracks, offwidths. Does a lot of hiking and trail running.</p><p>Total gear nerd.</p><p>10+ years climbing, 5 guiding</p><p>Coded this site. Also sews.</p></>,
+        bio: () => (
+            <>
+                <p>Originally from the humidity and short polished limestone of ATX and the JTree-like granite of Enchanted Rock... once he got a taste of what the great sandstone of Red Rock could offer, he knew he had to make it his home.</p>
+                <p>Loves easy multipitch, cracks, offwidths. Does a lot of hiking and trail running.</p>
+                <p>Total gear nerd.</p>
+                <p>10+ years climbing, 5 guiding</p>
+                <p>Coded this site. Also sews.</p>
+            </>
+        ),
         imgSrc: T2,
         ig: "https://www.instagram.com/climbguidecraft/",
         github: 'https://github.com/j-poneil',
@@ -109,7 +136,7 @@ export const staff = [
     {
         name: "Adam Happensack",
         position: "",
-        bio: () => <></>,
+        bio: () => (<></>),
         imgSrc: "https://via.placeholder.com/500x400",
         ig: "https://www.instagram.com/a.happensack/",
         site: '',
