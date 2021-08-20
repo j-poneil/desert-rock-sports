1b18e032157fae8313ceb4a052ba2e680ac25098
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-08-20 00:29:13 -0700

    created a handy template to fill out for each major accordion section and its sub-accordion sections. ACCORDION. HA.

---
+++ src/components/beta/sectionDataTemplate.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Animated } from 'react-animated-css';
+import { FaRegCaretSquareDown, FaRegCaretSquareUp } from 'react-icons/fa';
+import { MdExpandMore, MdExpandLess } from 'react-icons/md';
+
+
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+
+// icons
+// Standard?
+// FaExternalLinkAlt AFTER links away
+// FaDirections BEFORE Gmaps links away
+import { FaExternalLinkAlt, FaDirections, FaMapMarked, FaMapMarkedAlt, FaMapMarkerAlt, FaMap, FaPhone } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+
+
+// photo imports
+// import Photo from '..something../img/beta/photo.jpg';
+
+// % What this is supposed to have based on notes in beta.js
+//! - FOR EXAMPLE ONLY
+// Food
+        // Grocery Stores
+        // Coffee Shops
+        // Restraunts
+        // Breweries / Bars
+
+{/* <AccordionOfAccordions
+    title='Test Title'
+    accordionIdBase='testAccordion'
+    eventKeyBase='testEventKey'
+    subSectionArrayOfObjects={ templatePropsTestSections }
+/> */}
+// access like food.title, food.accordionIdBase, food.eventKeyBase, food.data
+// 'foodAccordion' ==> 'foodAccordionId0' ... 'foodEKey' ==> 'foodEKey0'
+
+// the parent of subSectionContent is <Card.Body></Card.Body>
+// So Card.Text and Card.Subtitle are logical tags to use
+
+export const food = {
+    title: 'Grocery, Food, and Drink Options',
+    accordionIdBase: 'foodAccordion',
+    eventKeyBase: 'foodEKey',
+    data: [
+        {
+            subSectionTitle: '1st subSection Title',
+            subSectionContent: () => (
+                <>
+                </>
+            )
+        },
+    ]
+};
+
