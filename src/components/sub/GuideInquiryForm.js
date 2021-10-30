09e84b5846637a5ea7387c7d2f3ea02eac9bea33
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2021-10-30 11:21:07 -0700

    converting guiding inquiry form to react-bootstrap components

--- src/components/sub/GuideInquiryForm.js
+++ src/components/sub/GuideInquiryForm.js
@@ -38,22 +38,61 @@ function GuideInquiryForm() {
             <Card.Body>
                 <Card.Title>Contact us!</Card.Title>
                 <Card.Text>
-                    <form action="https://formsubmit.co/redrockguides@gmail.com" method="POST">
-                        <input type="text" name="name" placeholder="Your name" value={state.name} onChange={handleChange} required />
-                        <input type="tel" name="tel" placeholder="Phone number" required />
-                        <input type="email" name="email" placeholder="youremailaddress@something.com" required />
-                        <input type="date" name="date" />
+                    <Form action="https://formsubmit.co/redrockguides@gmail.com" method="POST">
+                        {/* <input type="text" name="name" placeholder="Your name" value={state.name} onChange={handleChange} required /> */}
+                        <Form.Group controlId='formName'>
+                            <Form.Label>Full Name (or Names)</Form.Label>
+                            <Form.Control type="text" name="name" placeholder="Your name" value={state.name} onChange={handleChange} required />
+                        </Form.Group>
+
+                        {/* <input type="tel" name="tel" placeholder="Phone number" required /> */}
+                        <Form.Group controlId='formTel'>
+                            <Form.Label>Best phone number to reach you at</Form.Label>
+                            <Form.Control type="tel" name="tel" placeholder="Phone number" required />
+                        </Form.Group>
+
+                        {/* <input type="email" name="email" placeholder="youremailaddress@something.com" required /> */}
+                        <Form.Group controlId='formEmail'>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" name="email" placeholder="youremailaddress@something.com" required />
+                        </Form.Group>
+
+
+                        <Form.Group controlId=''>
+                            <Form.Label></Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+
+                        <Form.Group controlId=''>
+                            <Form.Label></Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        {/* <textarea name="details" placeholder="Give us the details!" rows="10" required /> */}
+                        <Form.Group controlId=''>
+                            <Form.Label></Form.Label>
+                            <Form.Control as='textarea' name="details" placeholder="Give us more details! What are your goals? What are your experience level(s)? Age(s)? Are there particular logistical difficulties associated with these dates? Such as hard deadlines you need to be back in Vegas by to catch a flight. Is other contact info needed? Whats the best way to contact you? Is there a particular route or routes you are interested in? Are there particular skills you are wanting to learn? etc..." rows={5} required />
+                        </Form.Group>
+
+
+                        {/* <Form.Group controlId=''>
+                            <Form.Label></Form.Label>
+                            <Form.Control />
+                        </Form.Group> */}
+                        
+                        {/* <input type="date" name="date" /> */}
                         <input type="checkbox" name="T1" />
                         <input type="checkbox" name="T2" />
                         <input type="checkbox" name="T3" />
-                        <textarea name="message" placeholder="Give us the details!" rows="10" required />
+                        
                         {/* Hidden inputs */}
                         <input type="hidden" name="_subject" value="New guiding inquiry via web form" />
                         <input type="hidden" name="_template" value="table" />
                         <input type="text" name="_honey" style={{'display': 'none'}} />
                         {/* Submit button */}
                         <Button type="submit">Send</Button>
-                    </form>
+                    </Form>
                 </Card.Text>
             </Card.Body>
         </Card>
