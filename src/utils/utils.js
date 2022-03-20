40bfcdf4249794260efcbfcb0466d37a568794b2
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2022-03-20 00:40:54 -0700

    Further readability improvements on gallery title / sub-title / description text

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,7 +13,19 @@ function styleModalTitle(text){
                 1px -1px 0 #000,
                 -1px 1px 0 #000,
                 1px 1px 0 #000
-            `
+            `,
+            // This adds a transparent black background, looks OK
+            backgroundColor: `rgba(0,0,0,0.5)`,
+            padding: '2px',
+            // radius only seems to effect the beginning and end of the full portion of wrapped text when not display: 'inline-block'
+            // so benefit to using it, even if I don't mess with restricting width
+            borderRadius: '5px',
+            // restrict the width?
+            // span is an inline element by default, thus no width or height
+            // unless you set it to display differently
+            display: 'inline-block',
+            // maxWidth: '75%',
+            // width: '50%'
         }}
     >
         { text }
