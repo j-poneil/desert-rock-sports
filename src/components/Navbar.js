2e55528c462ad0e935b9121069add3596513947a
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2022-04-12 22:22:06 -0700

    commented out link to gym in nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ export default function Navbar(){
                     <Nav.Link href="/gear">Gear</Nav.Link>
                     <Nav.Link href="/beta">Beta</Nav.Link>
                     <Nav.Link href="/weather">Weather</Nav.Link>
-                    <Nav.Link href="https://www.redrockclimbingcenter.com/" target="_blank" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Opens in a new tab">Gym <sup><FaExternalLinkAlt /></sup></Nav.Link>
+                    {/* <Nav.Link href="https://www.redrockclimbingcenter.com/" target="_blank" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Opens in a new tab">Gym <sup><FaExternalLinkAlt /></sup></Nav.Link> */}
                     <Nav.Link href="/guiding">Guiding</Nav.Link>
                     <Nav.Link href="/resoles">Resoles</Nav.Link>
                     {/* <Nav.Link href="/other">Other</Nav.Link> */}
