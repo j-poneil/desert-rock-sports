744edd0bbc81e40eef5583c1039ebad24bbb4c88
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2023-06-01 13:56:15 -0700

    notes

--- src/components/Secret.js
+++ src/components/Secret.js
@@ -6,6 +6,7 @@ import TheVerm from '../img/secret/the_verm.jpg';
 
 
 // Idea is to have a hidden page with climbing memes, tributes, and whatever else. Probably flexbox and overflow wrap. Would be nice to have it auto populate using images that are placed into a folder, doing auto resize so they try to use original img size up to a certain max.
+// ... but yeah, that doesn't matter. This page doesn't matter.
 export default function Secret(){
     return (
         <div className="secret-flex-container">
