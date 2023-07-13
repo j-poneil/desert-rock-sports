68256888ad5074ab0bf94a966f3f4d1ca6e338dc
John-Paul (Travis) O'Neil <j-poneil@users.noreply.github.com>
2023-07-12 19:28:52 -0700

    initial commit, entry point for Plotly.js

---
+++ src/components/sub/PlotlyWxRRChart.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//! In the future, consider making a custom dist with just the required chart types
+//!!! -- 3.6MB
+// import Plotly from 'plotly.js-dist-min';
+import Plot from 'react-plotly.js';
+
+const PlotlyWxRRChart = () => {
+  return (
+    <div>
+        {/*  */}
+    </div>
+  )
+}
+
+export default PlotlyWxRRChart;
